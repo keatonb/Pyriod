35823dba31197d12f43d970dd71743d878404160
keatonb <keatonbell@utexas.edu>
2021-05-15 16:27:07 -0700

    v0.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="Pyriod",
-    version="0.0.10",
+    version="0.0.11",
     author="Keaton Bell",
     author_email="keatonbell@utexas.edu",
     description="Basic period detection and fitting routines for astronomical time series.",
